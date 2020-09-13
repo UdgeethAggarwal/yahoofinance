8021630069b9ee5819c11bd3dadbf8a50a5ce46c
UdgeethAggarwal <udgeeth.aggarwal@gmail.com>
2020-09-14 02:30:17 +0530

    Keys removed

--- northloop/app/controllers/stockController.js
+++ northloop/app/controllers/stockController.js
@@ -1,14 +1,14 @@
 var request = require('request');
 
-
+//Add your host and key
 exports.fetchStockAnalysis = (req, res) => {
   request({
     uri: 'https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-analysis',
     headers: {
    'Accept': 'application/json',
    'Content-Type': 'application/json',
-   'X-RapidAPI-Host':'apidojo-yahoo-finance-v1.p.rapidapi.com',
-   'X-RapidAPI-Key':'4c3a864cf5mshfb34c1cd71949c3p175a2cjsnb87f19241666'
+   'X-RapidAPI-Host':'',
+   'X-RapidAPI-Key':''
  },
     qs: {
       region: "US",
